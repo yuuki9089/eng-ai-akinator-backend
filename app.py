d6d85dd7a0a154ff853c1a1d857c4b82e3046aca
yuuki9089 <yuukiou90@gmail.com>
2025-09-29 01:07:24 +0900

    Add: キャラクタ一覧と質問をDBから取得するAPI

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 import db_ctl
+import random
 from starlette.middleware.cors import CORSMiddleware # 追加
 
 app = FastAPI()
@@ -22,9 +23,35 @@ async def root():
 async def ping():
     return {"message":"pong"}
 
+# ジャンル一覧を取得するAPI
 @app.get("/genres")
 async def genres():
     return db_ctl.select_genres()
+
+# キャラクタ一覧を取得するAPI
+@app.get("/characters")
+async def characters():
+    return db_ctl.select_characters()
+
+# 質問一覧を取得するAPI
+@app.get("/questions")
+async def questions():
+    return db_ctl.select_questions()
+
+# 質問一覧からランダムで3つ質問を取得するAPI
+@app.get("/random_questions")
+async def random_questions():
+    # DBから質問一覧を取得
+    all_questions = db_ctl.select_questions()
+    # 質問の数の範囲内で乱数を重複無しで取得
+    extracted_nums = random.sample(range(0,len(all_questions)),3)
+    print(extracted_nums)
+    # 該当の番号の質問を取得
+    extracted_questions = []
+    for num in extracted_nums:
+        extracted_questions.append(all_questions[num])
+    return extracted_questions
+
 
 if __name__ == "__main__":
     uvicorn.run(app,host="127.0.0.1",port=8000, log_level="debug")
--- db_ctl.py
+++ db_ctl.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 # --- .envファイルの読み込み ---
 load_dotenv()
 
-# --- DBへの接続 ---
+# =================================
+# 【DBへの接続】
+# =================================
 try:
     conn = mysql.connector.connect(
         user=os.environ['DB_USER'],
@@ -20,7 +22,10 @@ except:
 # 取得結果を辞書型で扱う設定
 cur = conn.cursor(dictionary=True)
 
-# ジャンルマスタからジャンルを取得する関数
+
+# =================================
+# 【ジャンルマスタからジャンルを取得する関数】
+# =================================
 def select_genres():
     query__for_fetching = """
     SELECT
@@ -33,10 +38,52 @@ def select_genres():
     cur.execute(query__for_fetching)
     return cur.fetchall()
 
+# =================================
+# 【キャラクタマスタからキャラクタを取得する関数】
+# =================================
+def select_characters():
+    query__for_fetching = """
+    SELECT
+          *
+    FROM キャラクタマスタ
+    ORDER BY id
+    ;
+    """
+    cur.execute(query__for_fetching)
+    return cur.fetchall()
 
+# =================================
+# 【質問マスタから質問を取得する関数】
+# =================================
+def select_questions():
+    query__for_fetching = """
+    SELECT
+          *
+    FROM 質問マスタ
+    ORDER BY question_id
+    ;
+    """
+    cur.execute(query__for_fetching)
+    return cur.fetchall()
+
+# =================================
+# 【メイン関数】
+# =================================
 if __name__ == "__main__":
     result = select_genres()
     for fetched_line in result:
         genre_code = fetched_line['genre_code']
         genre_name = fetched_line['genre_name']
         print(f'{genre_code}: {genre_name}')
+
+    result = select_characters()
+    for fetched_line in result:
+        genre_code = fetched_line['id']
+        genre_name = fetched_line['character_name']
+        print(f'{genre_code}: {genre_name}')
+
+    result = select_questions()
+    for fetched_line in result:
+        question_code = fetched_line['question_id']
+        question_content = fetched_line['question_content']
+        print(f'{question_code}: {question_content}')
